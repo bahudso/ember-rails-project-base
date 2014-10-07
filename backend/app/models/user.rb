20d09512670c09a7c0b3fd6331081911e67f78cb
Ben Hudson <bahudso@g.clemson.edu>
2014-10-07 16:03:32 -0400

    Configure rails backend for simple-auth-devise.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+	before_save :ensure_authentication_token
+
+	def ensure_authentication_token
+		if authentication_token.blank?
+		  self.authentication_token = generate_authentication_token
+		end
+	end
+
+	private
+
+		def generate_authentication_token
+		  loop do
+		    token = Devise.friendly_token
+		    break token unless User.where(authentication_token: token).first
+		  end
+		end
 end
